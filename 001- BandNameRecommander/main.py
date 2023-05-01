e75f79419a769461c2f32afc175f70f228748f47
Walid Sharaar <walidsharar@gmail.com>
2023-05-01 10:51:15 +0200

    Previous projects

---
+++ 001- BandNameRecommander/main.py
@@ -0,0 +1,15 @@
+# Create a greeting for the program
+
+print("Welcome to the recommendar system: ")
+
+# Ask the user for the city that they grew up in.
+
+city  = input ("Please the name of your city: ")
+
+# Ask the user for the name of their beloved pet
+
+pet = input(" Enter your beloved pet name: \n")
+
+# Combine the name of their  city and pet to show the band name.
+
+print("Your band name would be : " + city+" " +pet )
